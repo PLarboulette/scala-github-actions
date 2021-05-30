a4c6dd435399ed549f1e61f23203d2d3253220af
pierrelarboulette <pierre.larboulette@iadvize.com>
2021-05-30 13:39:03 +0200

    feat(config): test with a Redis container

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ version := "0.1"
 
 scalaVersion := "2.13.6"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % Test
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.1.0" % Test,
+  "net.debasishg" %% "redisclient" % "3.30"
+)
 
 coverageEnabled := true
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,7 +1,16 @@
 import org.scalatest.funsuite.AnyFunSuite
+import com.redis._
 
 class MainTest extends AnyFunSuite {
+
   test("Example test") {
     assert(1 == 1)
+  }
+
+  test("Redis") {
+    val r = new RedisClient("localhost", 6379)
+    r.set("key", "some value")
+    val res = r.get("key")
+    assert(res.get == "some value")
   }
 }
